fd0bc476176515a772aba04b9f5138379eedb079
lucy0246 <lucy0246@stud.kea.dk>
2022-01-31 12:06:56 +0100

    interaktiv

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-//øvelse 1 og 2//
+//øvelse 1//
 /* let længde = 3;
 let bredde = 50;
 let areal = længde * bredde;
-let resultat;
-
+let resultat =
+  "længden er " +
+  længde +
+  "meter og bredden er " +
+  bredde +
+  " meter. Arealet er " +
+  areal;
+console.log(resultat);
+//øvelse 2//
 if (areal < 100) {
   console.log("arealet er for lille ");
 } else if (areal < 200) {
@@ -11,9 +18,23 @@ if (areal < 100) {
 } else {
   console.log("arealet er for stort");
 } */
+
 //øvelse 3//
-document.querySelector("button").addEventListener("click", buttonclicked);
+/* document.querySelector("button").addEventListener("click", buttonclicked);
 function buttonclicked() {
   let brugernavn = document.querySelector("#navn").value;
   console.log("goddag " + brugernavn);
-}
+} */
+let længde = document.querySelector("#længde").value;
+let bredde = document.querySelector("#bredde").value;
+let areal = længde * bredde;
+let resultat =
+  " længden er " +
+  længde +
+  " meter og bredden er " +
+  bredde +
+  " meter. Arealet er " +
+  areal;
+console.log(resultat);
+document.querySelector("button").addEventListener("click", buttonclicked);
+function buttonclicked() {}
